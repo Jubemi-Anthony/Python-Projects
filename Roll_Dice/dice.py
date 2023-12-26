85cb3b6b1e4e1573358b6ffc0e4e02cfb77f9c96
Jubemi-Anthony <jubemipajiah@gmail.com>
2023-12-26 18:53:08 +0100

    Dice roller

---
+++ Roll_Dice/dice.py
@@ -0,0 +1,14 @@
+import random
+
+def roll_dice():
+    roll = input("Roll Dice? (Yes/No)")
+
+    while roll.lower() == 'yes':
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1,6)
+
+        print(f"Dice rolled: {dice1} and {dice2}")
+
+        roll = input("Roll again? (Yes/No): ")
+
+roll_dice()
